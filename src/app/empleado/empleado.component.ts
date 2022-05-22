01b8ab834cc446c79dfbd871b4388d71cec78129
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-05-22 17:22:04 -0300

    interpolación

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
+  //creo las propiedades
+
+  nombre="Gimena";
+  apellido="Binaghi";
+  //private edad=22;
+  edad=32;
+  empresa="Google";
+
+  /*getEdad(){
+    return this.edad;
+  }*/
+
+
   constructor() { }
 
   ngOnInit(): void {
